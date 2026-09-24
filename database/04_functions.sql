9ea6c1207561e25a14b9618fdd0d4a8942139d56
Sajin P <sajin7099@gmail.com>
2026-09-24 21:57:48 +0530

    Add ACWR calculation function to SQL script
    Added a new function to calculate ACWR based on acute and chronic workload.

--- database/04_functions.sql
+++ database/04_functions.sql
@@ -1,3 +1,5 @@
+#WORKLOAD FUNCTION
+    
 CREATE OR REPLACE FUNCTION calculate_workload(
     p_duration NUMBER,
     p_rpe NUMBER
@@ -6,5 +8,21 @@ RETURN NUMBER
 IS
 BEGIN
     RETURN p_duration * p_rpe;
+END;
+/
+
+#ACWR Function
+CREATE OR REPLACE FUNCTION calculate_acwr(
+    p_acute NUMBER,
+    p_chronic NUMBER
+)
+RETURN NUMBER
+IS
+BEGIN
+    IF p_chronic = 0 THEN
+        RETURN NULL;
+    END IF;
+
+    RETURN ROUND(p_acute / p_chronic, 2);
 END;
 /
